51a54aaed0eb02139dcc8d0eb6292f6fe67abdc1
Sanjay Dey <sanjay.dey@konasl.com>
2022-06-02 22:18:44 +0600

    action added for save , copy and share

--- gallary/monstar-gallary/BaseViewController.swift
+++ gallary/monstar-gallary/BaseViewController.swift
@@ -78,6 +78,12 @@ class BaseViewController: UIViewController {
         
     }
     
+    func showAlert(title: String, message: String){
+            let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
+    
 
     
 
--- gallary/monstar-gallary/GallaryViewer/PhotoViewController.swift
+++ gallary/monstar-gallary/GallaryViewer/PhotoViewController.swift
@@ -6,13 +6,14 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PhotoViewController: BaseViewController {
   
     
     @IBOutlet weak var menuButton: UIBarButtonItem!
     
-    @IBOutlet weak var button: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         if #available(iOS 14.0, *) {
@@ -33,7 +34,6 @@ class PhotoViewController: BaseViewController {
     
     
     @IBAction func onOptionButtonClick(_ sender: Any) {
-           let menu =  makeMenu()
        
     }
     
@@ -43,32 +43,51 @@ class PhotoViewController: BaseViewController {
 
     func makeMenu() -> UIMenu {
         let menu = UIMenu(title: "", options: .displayInline, children: [
-            UIAction(title: "save", image: UIImage(systemName: "square.and.arrow.down"),handler: {_ in
-                print("save")
+            UIAction(title: "save", image: UIImage(systemName: "square.and.arrow.down"),handler: {
+                [weak self] (_)in
+                self?.savePhoto()
             }),
-            UIAction(title: "copy", image: UIImage(systemName: "doc.on.doc"),handler: {_ in
-                print("copy")
-                let image = UIImage(named: "appstore.png")
-                UIPasteboard.general.image = image;
-                self.showToast(message: "Copied")
-                
-                
+            UIAction(title: "copy", image: UIImage(systemName: "doc.on.doc"),handler: {
+                 [weak self](_) in
+                self?.copyPhoto()
             }),
-            UIAction(title: "share", image: UIImage(systemName: "square.and.arrow.up"),handler: {_ in
-                print("share")
-                let image = UIImage(named: "appstore.png")
-                       
-               let imageToShare = [ image! ]
-               let activityViewController = UIActivityViewController(activityItems: imageToShare, applicationActivities: nil)
-               activityViewController.popoverPresentationController?.sourceView = self.view
-               
-               activityViewController.excludedActivityTypes = [ UIActivity.ActivityType.airDrop, UIActivity.ActivityType.postToFacebook ]
-               
-               self.present(activityViewController, animated: true, completion: nil)
+            UIAction(title: "share", image: UIImage(systemName: "square.and.arrow.up"),handler: { [weak self](_) in
+                self?.sharePhoto()
             })
             
         ])
         return menu
+    }
+    
+    func sharePhoto(){
+       let image = UIImage(named: "appstore.png")
+               
+       let imageToShare = [ image! ]
+       let activityViewController = UIActivityViewController(activityItems: imageToShare, applicationActivities: nil)
+       activityViewController.popoverPresentationController?.sourceView = self.view
+       
+       activityViewController.excludedActivityTypes = [ UIActivity.ActivityType.airDrop, UIActivity.ActivityType.postToFacebook ]
+       
+       self.present(activityViewController, animated: true, completion: nil)
+    }
+    
+    func copyPhoto(){
+        let image = UIImage(named: "appstore.png")
+        UIPasteboard.general.image = image;
+        self.showToast(message: "Copied")
+    }
+    
+    func savePhoto(){
+        UIImageWriteToSavedPhotosAlbum(UIImage(named: "appstore.png")!, self, #selector(self.image(_:didFinishSavingWithError:contextInfo:)), nil)
+    }
+    
+    @objc func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+            if let error = error {
+                // we got back an error!
+                showAlert(title: "Save error", message: error.localizedDescription)
+            } else {
+                showAlert(title: "Saved!", message: "Your image has been saved to your photos.")
+            }
     }
     
     
